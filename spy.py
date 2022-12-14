60b6dc3ad8a2d27ee82c85418268eb85e1720022
Hatef Barin <amir.barin76@gmail.com>
2022-12-14 15:56:54 +0330

    added Game's logic in spy.py

--- spy.py
+++ spy.py
@@ -1,6 +1,42 @@
-import tkinter as tk
-from tkinter import ttk
+import random
 
 
-class SpyGame:
-    pass
+class Spy:
+    def __init__(self, word):
+        self.players_list = []
+        # Player's number
+        self.players_quantity = None
+        # Peoples
+        self.peoples = None
+        # Spies
+        self.spies = None
+        # Create a word
+        self.word = random.choice(word)
+
+    def create_player(self):
+        # Add Players and Spies
+        self.players_quantity = int(input('How many Players want to play? 4, 6, 8 ====> '))
+        if self.players_quantity == 4:
+            self.spies = 1
+        elif self.players_quantity == 6 or 8:
+            self.spies = int(input('How many Spy? 1, 2, 3 ====> '))
+        self.peoples = self.players_quantity - self.spies
+
+        # Create Players list
+        for people in range(self.peoples):
+            self.players_list.append('People')
+
+        for spy in range(self.spies):
+            self.players_list.append('Spy')
+
+        # Game start
+        for turn in range(len(self.players_list)):
+            character = random.choice(self.players_list)
+            if character == 'People':
+                print(f'You are a people!\nThe word is {self.word}')
+            else:
+                print('You are a Spy!')
+            self.players_list.remove(character)
+
+            if len(self.players_list) == 0:
+                print('The Game has started')
